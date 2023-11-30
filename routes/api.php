72ccc059fffa36ce9967c6f297abe53409e1136c
ialdaverag <ialdaverag@outlook.es>
2023-11-30 17:26:14 -0600

    add the /post/{id} route [GET]

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::group(['middleware' => 'api', 'prefix' => 'community'], function ($router
 Route::group(['middleware' => 'api', 'prefix' => 'post'], function ($router) {
     Route::post('/', [PostController::class, 'store']);
     Route::get('/', [PostController::class, 'index']);
-    // Route::get('/{post:id}', [PostController::class, 'show']);
+    Route::get('/{post:id}', [PostController::class, 'show']);
     // Route::patch('/{post:id}', [PostController::class, 'update']);
     // Route::delete('/{post:id}', [PostController::class, 'destroy']);
 });
